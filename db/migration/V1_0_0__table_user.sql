3484de9c876432fd51c0e0451b67c5d62323f5c6
Cai The Si <caithesi21041997@gmail.com>
2024-09-29 21:49:51 +0700

    fix migrate file

--- db/migration/V1_0_0__table_user.sql
+++ db/migration/V1_0_0__table_user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `user` (
     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(20),
+    `username` varchar(20),
     `password` varchar(20)
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8;
